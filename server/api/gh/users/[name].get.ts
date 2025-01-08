c052d9202612172ebee9882664a1bc1dfc25744a
Gerben Mulder <github.undergo381@passmail.net>
2025-01-08 03:52:38 +0100

    fuck this shit

--- server/api/gh/users/[name].get.ts
+++ server/api/gh/users/[name].get.ts
@@ -8,52 +8,93 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const user = await getUser(name);
+  const user = users[name as keyof typeof users];
+
+  if (!user) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'User not found',
+    });
+  }
+
   return {
     id: user.id,
-    username: user.login,
+    username: user.username,
     name: user.name,
-    twitter: user.twitter_username,
-    avatar: user.avatar_url,
+    twitter: user.twitter,
+    avatar: user.avatar,
 
   };
 });
 
-interface GithubUser {
-  id: number;
-  login: string;
-  name: string;
-  twitter_username: string;
-  avatar_url: string;
-}
+// interface GithubUser {
+//   id: number;
+//   login: string;
+//   name: string;
+//   twitter_username: string;
+//   avatar_url: string;
+// }
 
-const getUser = defineCachedFunction(async (name: string) => {
-  const token = useRuntimeConfig().githubToken;
-  return $fetch<GithubUser>(`https://api.github.com/users/${name}`, {
-    method: 'GET',
-    headers: {
-      'User-Agent': 'fetch',
-      'Authorization': `Bearer ${token}`,
-    },
-  });
-}, {
-  group: 'gh',
-  swr: false,
-  maxAge: 60 * 60 * 6, // 6 hours
-  staleMaxAge: 60 * 60 * 12, // 12 hours
-  validate(entry) {
-    if (
-      !entry.value
-      || isEmptyArray(entry.value)
-      || entry.value?.total_count === 0
-      || isEmptyArray(entry.value?.items)
-    ) {
-      return false;
-    }
-    return true;
+// const getUser = defineCachedFunction(async (name: string) => {
+//   const token = useRuntimeConfig().githubToken;
+//   return $fetch<GithubUser>(`https://api.github.com/users/${name}`, {
+//     method: 'GET',
+//     headers: {
+//       'User-Agent': 'fetch',
+//       'Authorization': `Bearer ${token}`,
+//     },
+//   });
+// }, {
+//   group: 'gh',
+//   swr: false,
+//   maxAge: 60 * 60 * 6, // 6 hours
+//   staleMaxAge: 60 * 60 * 12, // 12 hours
+//   validate(entry) {
+//     if (
+//       !entry.value
+//       || isEmptyArray(entry.value)
+//       || entry.value?.total_count === 0
+//       || isEmptyArray(entry.value?.items)
+//     ) {
+//       return false;
+//     }
+//     return true;
+//   },
+// });
+
+// function isEmptyArray(val: unknown) {
+//   return Array.isArray(val) && val.length === 0;
+// }
+
+// Fuck this shit. Nuxt/Nitro is not loading the runtime config in cloudflare. So here is a hardcoded list of users.
+// TODO: Fix this shit.
+const users = {
+  atinux: {
+    id: 904724,
+    username: 'atinux',
+    name: 'Sébastien Chopin',
+    twitter: 'Atinux',
+    avatar: 'https://avatars.githubusercontent.com/u/904724?v=4',
   },
-});
-
-function isEmptyArray(val: unknown) {
-  return Array.isArray(val) && val.length === 0;
-}
+  egoist: {
+    id: 8784712,
+    username: 'egoist',
+    name: 'EGOIST',
+    twitter: 'localhost_5173',
+    avatar: 'https://avatars.githubusercontent.com/u/8784712?v=4',
+  },
+  zernonia: {
+    id: 59365435,
+    username: 'zernonia',
+    name: 'zernonia',
+    twitter: 'zernonia',
+    avatar: 'https://avatars.githubusercontent.com/u/59365435?v=4',
+  },
+  danielroe: {
+    id: 28706372,
+    username: 'danielroe',
+    name: 'Daniel Roe',
+    twitter: null,
+    avatar: 'https://avatars.githubusercontent.com/u/28706372?v=4',
+  },
+};
